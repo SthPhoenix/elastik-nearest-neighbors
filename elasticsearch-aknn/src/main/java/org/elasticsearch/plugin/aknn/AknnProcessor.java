d12393b82da9322729850c2e49a0cbd0d932e0a2
SthPhoenix <veloce-raptor@bk.ru>
2019-08-10 21:53:24 +0300

    Fix copyright

--- elasticsearch-aknn/src/main/java/org/elasticsearch/plugin/aknn/AknnProcessor.java
+++ elasticsearch-aknn/src/main/java/org/elasticsearch/plugin/aknn/AknnProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright [2018] [Alex Klibisz]
+ * Copyright [2019] [SthPhoenix]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
